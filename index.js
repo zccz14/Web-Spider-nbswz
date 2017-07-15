46c35b300d9e76e17d32159fa0e4022cc2dff0f9
zccz14 <zccz14@outlook.com>
2017-07-15 11:04:07 -0700

    ban export because of performance issue

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Fetch = require('./fetch');
-const Export = require('./export');
+// const Export = require('./export');
 const http = require('http');
 const config = require('./config.json');
 
 setImmediate(Fetch);
 setInterval(Fetch, config.interval);
 
-const port = process.env.PORT || '1203';
-http.createServer(Export).listen(port);
+// const port = process.env.PORT || '1203';
+// http.createServer(Export).listen(port);
